1a2b479cf4ba3e2b9489bbd7a5943b40919ae147
Pipa <damianpeaf@gmail.com>
2020-12-31 14:32:19 -0600

    Empezando a trabajar algunas cosas

---
+++ Datos/Conexion.cs
@@ -0,0 +1,42 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class Conexion
+    {
+
+        private MySqlConnection conexion = new MySqlConnection("Server=127.0.0.1;Uid=root;pwd=;database=");
+
+        public MySqlConnection IniciarConexion()
+        {
+
+            try
+            {
+                if (conexion.State == ConnectionState.Closed)
+                {
+                    conexion.Open();
+                }
+                return conexion;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Error al conectar a la base de datos: " + ex);
+                return null;
+            }
+        }
+
+        public MySqlConnection CerrarConexion()
+        {
+            if (conexion.State == ConnectionState.Open)
+                conexion.Close();
+            return conexion;
+        }
+
+    }
+}
